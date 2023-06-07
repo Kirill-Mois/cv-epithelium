963db857d3fff6e1e710974165aeee539c3fcad4
Kirill-Mois <55047341+Kirill-Mois@users.noreply.github.com>
2023-06-07 07:04:36 +0300

    Update func.py

--- func.py
+++ func.py
@@ -2,47 +2,59 @@ import cv2
 import numpy as np
 
 
-def get_area(epit_img):
-    # scale /= 1000
-    original_img = np.array(epit_img.convert('RGB'))
+def get_area(image):
+    original_img = np.array(image.convert('RGB'))
+    # Convert the image to the HSV color space
     hsv = cv2.cvtColor(original_img, cv2.COLOR_RGB2HSV)
+    # Define the lower and upper bounds for the red color range
     lower_red_1 = np.array([0, 80, 15])
     upper_red_1 = np.array([12, 255, 255])
-    mask1 = cv2.inRange(hsv, lower_red_1, upper_red_1)
     lower_red_2 = np.array([150, 80, 15])
     upper_red_2 = np.array([180, 255, 255])
-    mask2 = cv2.inRange(hsv, lower_red_2, upper_red_2)
-    res_mask = cv2.bitwise_or(mask1, mask2)
+    # Create a mask for the red color range
+    mask_1 = cv2.inRange(hsv, lower_red_1, upper_red_1)
+    mask_2 = cv2.inRange(hsv, lower_red_2, upper_red_2)
+    res_mask = cv2.bitwise_or(mask_1, mask_2)
+    # Count number of red pixels
     k_pixels = res_mask[res_mask > 0].shape[0]
-
     return k_pixels
 
 
-def get_contours(epit_img):
-    original_img = np.array(epit_img.convert('RGB'))
-    gray_img = cv2.cvtColor(original_img, cv2.COLOR_RGB2GRAY)
-    clahe = cv2.createCLAHE(clipLimit=5, tileGridSize=(10, 10))
-    clahe_gray_image = clahe.apply(gray_img)
-    # gaus_clahe_gray_image = gaussian_filter(clahe_gray_image, sigma=5)
-    gaus_clahe_gray_image = cv2.GaussianBlur(clahe_gray_image, (5, 5), 0)
-    ret, contr = cv2.threshold(gaus_clahe_gray_image, 95, 255, cv2.THRESH_BINARY)
-    contours, hierarchy = cv2.findContours(contr, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(original_img, contours, -1, (0, 255, 0), 3)
+def get_contours(image):
+    original_img = np.array(image.convert('RGB'))
+    # Create a mask for the red color range
+    hsv = cv2.cvtColor(original_img, cv2.COLOR_RGB2HSV)
+    lower_red_1 = np.array([0, 80, 15])
+    upper_red_1 = np.array([12, 255, 255])
+    lower_red_2 = np.array([150, 80, 15])
+    upper_red_2 = np.array([180, 255, 255])
+    mask_1 = cv2.inRange(hsv, lower_red_1, upper_red_1)
+    mask_2 = cv2.inRange(hsv, lower_red_2, upper_red_2)
+    res_mask = cv2.bitwise_or(mask_1, mask_2)
+    # Perform morphological operations to remove noise
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
+    processed_mask = res_mask.copy()
+    processed_mask = cv2.morphologyEx(processed_mask, cv2.MORPH_CLOSE, kernel)
+    processed_mask = cv2.morphologyEx(processed_mask, cv2.MORPH_OPEN, kernel)
+    # Apply Gaussian blur
+    blurred_mask = cv2.GaussianBlur(processed_mask, (5,5), 0)
+    # Find contours
+    contours, _ = cv2.findContours(blurred_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # Filter contours based on area
+    min_contour_area = 300
+    filtered_contours = []
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        if area > min_contour_area:
+            filtered_contours.append(contour)
+    # Draw contours on the image
+    cv2.drawContours(original_img, filtered_contours, -1, (0, 255, 0), 2)
     return original_img
 
 
-def mean_shift(epit_img):
-    original_img = np.array(epit_img.convert('RGB'))
-    spatialRadius = 35
-    colorRadius = 60
-    pyramidLevels = 3
-    res = cv2.pyrMeanShiftFiltering(original_img, spatialRadius, colorRadius, pyramidLevels)
-    return res
-
-
-def area_artefact(artefact_img):
-    artefact_img = np.array(artefact_img.convert('RGB'))
-    gray = cv2.cvtColor(artefact_img, cv2.COLOR_BGR2GRAY)
-    bin = cv2.threshold(gray, 80, 255, cv2.THRESH_BINARY)
-    return bin[bin > 0].shape[0]
-
+def get_artefacts(image):
+    original_img = np.array(image.convert('RGB'))
+    mean_shift = cv2.pyrMeanShiftFiltering(image, 32, 64, 2)
+    gray_mean_shift = cv2.cvtColor(mean_shift, cv2.COLOR_BGR2GRAY)
+    _, bin_mean_shift = cv2.threshold(gray_mean_shift, 64, 255, cv2.THRESH_BINARY)
+    return bin_mean_shift
